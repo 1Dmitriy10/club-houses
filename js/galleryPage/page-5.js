d803c13d895930ff120da57b7d7a71d57c5301d5
Дмитрий <1primo1@mail.ru>
2022-05-28 11:45:50 +0300

    update

---
+++ js/galleryPage/page-5.js
@@ -0,0 +1,198 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
+
+
+const pageBox = document.querySelector(".gallery__page-box")
+
+export function getPage5() {
+    pageBox.innerHTML = ""
+    pageBox.insertAdjacentHTML("beforeend", `
+    <div class="swiper gallery__slider">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper ff">
+        <!-- Slides -->
+        <div class="swiper-slide test"><img class="slider-img"
+                src="./assets/img/gallery/gallery-img-slider.png" alt=""></div>
+        <div class="swiper-slide test"><img class="slider-img"
+                src="./assets/img/gallery/gallery-img-slider.png" alt=""></div>
+        <div class="swiper-slide test"><img class="slider-img"
+                src="./assets/img/gallery/gallery-img-slider.png" alt="">
+        </div>
+        <div class="swiper-slide test"><img class="slider-img"
+                src="./assets/img/gallery/gallery-img-slider.png" alt=""></div>
+        <div class="swiper-slide test"><img class="slider-img"
+                src="./assets/img/gallery/gallery-img-slider.png" alt=""></div>
+
+    </div>
+
+    <!-- If we need navigation buttons -->
+    <div class="gallery-slider-prev"></div>
+    <div class="gallery-slider-next"></div>
+</div>
+
+
+
+<div class="swiper gallery__expert">
+    <!-- Additional required wrapper -->
+    <div class="swiper-wrapper">
+        <!-- Slides -->
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        <div class="swiper-slide gallery__expert-box">
+            <img src="./assets/img/gallery/gallery-expert.png" alt="img">
+            <div class="gallery__expert-text">
+                <h3 class="gallery__expert-title">Татьяна</h3>
+                <span class="gallery__expert-subtitle">Эксперт проекта</span>
+                <p class="gallery__expert-text">Knightsbridge Private Park — жилой квартал класса de
+                    luxe, расположенный в элитном районе Хамовники. Четыре семиэтажных особняка в
+                    Английском стиле с частным парком, аллеями, фонтаном, цветами и зонами отдыха.
+                    <br><br>
+
+                    Покупатели могут выбрать одноэтажную и двухэтажную квартиру площадью от 116 до
+                    505
+                    м², с потолками высотой до 3,6 метров. Просторные пентхаусы с террасами из
+                    натурального камня.
+                </p>
+            </div>
+
+        </div>
+        ...
+    </div>
+
+
+    <!-- If we need navigation buttons -->
+    <div class="gallery__expert-prev"></div>
+    <div class="gallery__expert-next">
+    </div>
+
+
+</div>
+`)
+    const gallery = new Swiper('.gallery__slider', {
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.gallery-slider-next',
+            prevEl: '.gallery-slider-prev',
+        },
+        loop: true,
+        slidesPerView: 2.2,
+        spaceBetween: 80,
+        centeredSlides: true,
+        transformEl: null
+
+
+    });
+    const expert = new Swiper('.gallery__expert', {
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.gallery__expert-next',
+            prevEl: '.gallery__expert-prev',
+        },
+        pagination: {
+
+            el: '.gallery__expert-pagination',
+            bulletElement: "div",
+            bulletClass: "bul",
+            dynamicBullets: true,
+            dynamicMainBullets: 3,
+        },
+        loop: true,
+        slidesPerView: 1,
+
+        centeredSlides: true,
+
+
+
+    });
+
+}
--- js/index.js
+++ js/index.js
@@ -1,50 +1,16 @@
-import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+
 import { animationCircle } from "./animation.js";
 import { info } from "./info.js";
 import { getMenu } from "./burgerMenu.js";
+import { getPage } from "./galleryPage/galleryPagination.js";
+import * as gallery from "./gallery.js";
+
 
 getMenu()
 info()
 animationCircle()
+getPage()
 
-const gallery = new Swiper('.gallery__slider', {
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.gallery-slider-next',
-        prevEl: '.gallery-slider-prev',
-    },
-    loop: true,
-    slidesPerView: 2.2,
-    spaceBetween: 80,
-    centeredSlides: true,
-    transformEl: null
-
-
-});
-const expert = new Swiper('.gallery__expert', {
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.gallery__expert-next',
-        prevEl: '.gallery__expert-prev',
-    },
-    pagination: {
-
-        el: '.gallery__expert-pagination',
-        bulletElement: "div",
-        bulletClass: "bul",
-        dynamicBullets: true,
-        dynamicMainBullets: 3,
-    },
-    loop: true,
-    slidesPerView: 1,
-
-    centeredSlides: true,
-
-
-
-});
 const presentation = new Swiper('.slider-top', {
 
     // Navigation arrows
